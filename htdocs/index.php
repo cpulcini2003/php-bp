26b404c29335666057811ed0d5ba73360105986a
Carlo Pulcini <cpulcini2003@gmail.com>
2023-02-24 11:35:36 +0100

    c5

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,7 @@
     <title>Powered By Paketo Buildpacks</title>
   </head>
   <body>
-    <?php echo '<h1>Hello WORLD</h1>'; ?>
+    <?php echo '<h1>Hello SOGEI</h1>'; ?>
     <?php phpinfo(); ?>
     <?php
       echo '<img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>';
